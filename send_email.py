535d8d7cee80f8a62381bd7ab7579879211b2292
fncize84 <94271624+fncize84@users.noreply.github.com>
2022-02-04 17:31:13 +0900

    Add files via upload

---
+++ send_email.py
@@ -0,0 +1,41 @@
+import smtplib 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication 
+
+def send_final():
+	sendEmail = "fncize@gmail.com"
+	recvEmail = "fncize@gmail.com"
+	password = "cswyifzraitnldfi"
+	smtpName = "smtp.gmail.com"
+	smtpPort = 587
+
+	msg = MIMEMultipart()
+
+	msg['Subject'] ="Weekly Trading Record"
+	msg['From'] = sendEmail 
+	msg['To'] = recvEmail 
+
+	text = "첨부."
+	contentPart = MIMEText(text) #MIMEText(text , _charset = "utf8")
+	msg.attach(contentPart) 
+
+	etcFileName = 'trading_record.xlsx'
+	with open(etcFileName, 'rb') as etcFD : 
+	    etcPart = MIMEApplication( etcFD.read() )
+	    etcPart.add_header('Content-Disposition','attachment', filename=etcFileName)
+	    msg.attach(etcPart) 
+    
+	s=smtplib.SMTP( smtpName , smtpPort )
+	s.starttls()
+	s.login( sendEmail , password ) 
+	s.sendmail( sendEmail, recvEmail, msg.as_string() )  
+	s.close()
+
+send_final()
+
+
+
+
+
+
